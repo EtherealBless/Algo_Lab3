bb25b66aab8d4a210b3434f77a362bd369143bd4
Your Name <you@example.com>
2024-11-08 23:47:40 +0500

    add stack do debug menu

--- Lab3/Menus/DebugMenu.cs
+++ Lab3/Menus/DebugMenu.cs
@@ -12,11 +12,13 @@ namespace Lab3.Menus
         Dictionary<int, Action> IMenu.Commands { get; set; } = new Dictionary<int, Action>()
         {
             { 1, TestLinkedListMenu.TestLinkedList },
+            {2, TestStackMenu.TestStack },
         };
 
         void IMenu.PrintMenu()
         {
             Console.WriteLine("1. Test LinkedList");
+            Console.WriteLine("2. Test Stack");
             Console.WriteLine("0. Exit");
         }
     }
