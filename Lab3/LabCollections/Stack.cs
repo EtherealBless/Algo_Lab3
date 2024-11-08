b71782c6a117f41d569ebd9d1d30d094a45f15f6
Your Name <you@example.com>
2024-11-08 23:33:52 +0500

    add stack

---
+++ Lab3/LabCollections/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.LabCollections
+{
+    internal class Stack
+    {
+        private LinkedList<object> list;
+        public Stack()
+        {
+            list = new LinkedList<object>();
+        }
+        public void Push(object obj)
+        {
+            list.Push(obj);
+        }
+        public object? Pop()
+        {
+            return list.PopBack();
+        }
+        public object? Top()
+        {
+            return list.Last();
+        }
+        public bool IsEmpty()
+        {
+            return list.IsEmpty();
+        }
+        public void Print()
+        {
+            list.Print();
+        }
+    }
+}
